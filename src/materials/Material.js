825f15b88004098de80e3fc5b9d26defd4a7feac
冷小凡(10029731) <lengxf@glodon.com>
2020-01-10 10:58:20 +0800

    Material

--- src/materials/Material.js
+++ src/materials/Material.js
@@ -8,37 +8,49 @@ import { _Math } from '../math/Math.js';
  */
 
 var materialId = 0;
-
+/**
+ * 材质的抽象基类
+ * 材质描述了对象objects的外观。它们的定义方式与渲染器无关， 因此，如果您决定使用不同的渲染器，不必重写材质
+ */
 function Material() {
 
-	Object.defineProperty( this, 'id', { value: materialId ++ } );
+	Object.defineProperty( this, 'id', { value: materialId ++ } );  // 此材质实例的唯一编号，只读
 
-	this.uuid = _Math.generateUUID();
+	this.uuid = _Math.generateUUID();  // 此材质实例的UUID，会自动分配，不应该被更改
 
-	this.name = '';
+	this.name = '';  // 对象的可选名称（不必是唯一的）。默认值为空字符串
 	this.type = 'Material';
 
-	this.fog = true;
+	this.fog = true;  // 材质是否受雾影响。默认为true
 
-	this.blending = NormalBlending;
-	this.side = FrontSide;
-	this.flatShading = false;
-	this.vertexTangents = false;
-	this.vertexColors = NoColors; // THREE.NoColors, THREE.VertexColors, THREE.FaceColors
+	this.blending = NormalBlending;  // 在使用此材质显示对象时要使用何种混合 
+	this.side = FrontSide;  // 定义将要渲染哪一面 - 正面，背面或两，默认为正面 
+	this.flatShading = false;  // 定义材质是否使用平面着色进行渲染。默认值为false 
+	this.vertexTangents = false;  // 
+	this.vertexColors = NoColors; // 是否使用顶点着色。默认值为THREE.NoColors。 其他选项有THREE.VertexColors 和 THREE.FaceColors 
 
-	this.opacity = 1;
-	this.transparent = false;
+	/**
+	 * 在0.0 - 1.0的范围内的浮点数，表明材质的透明度
+	 * 如果材质的transparent属性未设置为true，则材质将保持完全不透明，此值仅影响其颜色。 默认值为1.0
+	 */
+	this.opacity = 1; 
+	/**
+	 * 定义此材质是否透明，影响渲染，因为透明对象需要特殊处理，并在非透明对象之后渲染。
+	 * 设置为true时，通过设置材质的opacity属性来控制材质透明的程度。默认值为false
+	 */
+	this.transparent = false;  
 
-	this.blendSrc = SrcAlphaFactor;
-	this.blendDst = OneMinusSrcAlphaFactor;
-	this.blendEquation = AddEquation;
-	this.blendSrcAlpha = null;
-	this.blendDstAlpha = null;
-	this.blendEquationAlpha = null;
 
-	this.depthFunc = LessEqualDepth;
-	this.depthTest = true;
-	this.depthWrite = true;
+	this.blendSrc = SrcAlphaFactor;  // 混合源，必须将材质的blending设置为CustomBlending才能生效
+	this.blendDst = OneMinusSrcAlphaFactor;  // 混合目标，必须将材质的blending设置为CustomBlending才能生效
+	this.blendEquation = AddEquation;  // 使用混合时所采用的混合方程式，必须将材质的blending设置为CustomBlending才能生效
+	this.blendSrcAlpha = null;  // blendSrc的透明度。 默认值为 null
+	this.blendDstAlpha = null;  // blendDst的透明度。 默认值为 null
+	this.blendEquationAlpha = null;  // blendEquation 的透明度. 默认值为 null
+
+	this.depthFunc = LessEqualDepth;  // 使用何种深度函数
+	this.depthTest = true;  // 是否在渲染此材质时启用深度测试。默认为 true
+	this.depthWrite = true;  // 渲染此材质是否对深度缓冲区有任何影响。默认为true
 
 	this.stencilWriteMask = 0xff;
 	this.stencilFunc = AlwaysStencilFunc;
@@ -49,32 +61,36 @@ function Material() {
 	this.stencilZPass = KeepStencilOp;
 	this.stencilWrite = false;
 
+	/**
+	 * 用户定义的剪裁平面，在世界空间中指定为THREE.Plane对象
+	 * 这需要WebGLRenderer.localClippingEnabled为true
+	 */
 	this.clippingPlanes = null;
-	this.clipIntersection = false;
-	this.clipShadows = false;
+	this.clipIntersection = false;  // 更改剪裁平面的行为，以便仅剪切其交叉点，而不是它们的并集。默认值为 false
+	this.clipShadows = false;  // 定义是否根据此材质上指定的剪裁平面剪切阴影。默认值为 false
 
-	this.shadowSide = null;
+	this.shadowSide = null;  // 定义投影的面。设置时，可以是THREE.FrontSide, THREE.BackSide, 默认值为 null。
 
-	this.colorWrite = true;
+	this.colorWrite = true;  // 是否渲染材质的颜色。 这可以与网格的renderOrder属性结合使用，以创建遮挡其他对象的不可见对象。默认值为true
 
 	this.precision = null; // override the renderer's default precision for this material
 
-	this.polygonOffset = false;
-	this.polygonOffsetFactor = 0;
-	this.polygonOffsetUnits = 0;
+	this.polygonOffset = false;  // 是否使用多边形偏移。默认值为false。这对应于WebGL的GL_POLYGON_OFFSET_FILL功能
+	this.polygonOffsetFactor = 0;  // 设置多边形偏移系数。默认值为0
+	this.polygonOffsetUnits = 0;  // 设置多边形偏移单位。默认值为0
 
-	this.dithering = false;
+	this.dithering = false;  // 是否对颜色应用抖动以消除条带的外观。默认值为 false
 
-	this.alphaTest = 0;
-	this.premultipliedAlpha = false;
+	this.alphaTest = 0;  // 设置运行alphaTest时要使用的alpha值。如果不透明度低于此值，则不会渲染材质。默认值为0
+	this.premultipliedAlpha = false;  // 是否预乘alpha（透明度）值
 
-	this.visible = true;
+	this.visible = true;  // 此材质是否可见。默认为true
 
-	this.toneMapped = true;
+	this.toneMapped = true;  // 定义是否根据渲染器的toneMapping设置对该材质进行色调映射。默认为true。
 
-	this.userData = {};
+	this.userData = {};  // 一个对象，可用于存储有关Material的自定义数据。它不应该包含对函数的引用，因为这些函数不会被克隆
 
-	this.version = 0;
+	this.version = 0;  // 从0开始，并计数.needsUpdate：Boolean设置为true的次数。
 
 }
 
@@ -86,6 +102,10 @@ Material.prototype = Object.assign( Object.create( EventDispatcher.prototype ), 
 
 	onBeforeCompile: function () {},
 
+	/**
+	 * 通过value设置该对象的属性值
+	 * @param {object} values 
+	 */
 	setValues: function ( values ) {
 
 		if ( values === undefined ) return;
